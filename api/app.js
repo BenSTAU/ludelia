31be23ccec7cc18467f757cb5575de58667d2562
BenSTAU <ben.saint.augustin@gmail.com>
2025-08-28 11:46:21 +0200

    initialisation api et client

---
+++ api/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import pool from "./config.js";
+
+dotenv.config();
+
+app.use(cors(), {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+});
+app.use(express.json());
+
+const app = express();
+
+async function connect() {
+  await pool
+    .connect()
+    .then((client) => {
+      console.log("Connected to the database");
+      client.release();
+    })
+    .catch((err) => {
+      console.error("Error connecting to the database", err);
+    });
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+}
+connect();
