3798bd90f4af18b80d5aad26537458d8a19083b3
BenSTAU <ben.saint.augustin@gmail.com>
2025-08-29 08:39:51 +0200

    debut d'initialisation avec setup du server ainsi que création du footer nav et carte principale

--- api/app.js
+++ api/app.js
@@ -4,14 +4,15 @@ import dotenv from "dotenv";
 import pool from "./config.js";
 
 dotenv.config();
-
-app.use(cors(), {
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-});
-app.use(express.json());
-
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
 
 async function connect() {
   await pool
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-import Nav from "./component/Nav";
+import Card from "./component/card/Card";
+import Footer from "./component/footer/Footer";
+import Nav from "./component/nav/Nav";
 
 function App() {
   return (
     <>
       <Nav />
+      <main>
+        <Card
+          height="200px"
+          backgroundHeader={"var(--color-accent-1)"}
+          title={"Card Title"}
+          subtitle={"Card Subtitle"}
+        />
+      </main>
+      <Footer />
     </>
   );
 }
