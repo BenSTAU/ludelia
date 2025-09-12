25722511f46450f9b277eea87ac144a7144fe794
BenSTAU <ben.saint.augustin@gmail.com>
2025-09-12 12:08:03 +0200

    résolution bug googleAuth

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -2,7 +2,6 @@ import jwt from "jsonwebtoken";
 export default function verifyToken(req, res, next) {
   try {
     const token = req.cookies.token;
-    console.log("Token from cookies:", token);
     if (!token) {
       return res.status(401).json({ message: "Unauthorized" });
     }
